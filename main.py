110bd1b8c2f8697cba5c409818de26237d4cb467
samuelrahmatuel <cemtina.ily@gmail.com>
2023-04-01 12:07:45 +0700

    update

--- main.py
+++ main.py
@@ -11,5 +11,26 @@ def index():
         return render_template('main.html', text=text, prediction=prediction)
     return render_template('main.html')
 
+@app.route('/download', methods=['POST'])
+def download():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    list_username = ['a', 'b', 'c']
+    list_password = ['x', 'y', 'z']
+
+    if username in list_username:
+        id_user = list_username.index(username)
+
+        if password == list_password[id_user]:
+
+            return "Berhasil"
+        else:
+
+            return "password salah"
+    else:
+
+        return "username salah"
+
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
